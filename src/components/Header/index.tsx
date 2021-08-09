f99c18110867367387ce87a2e2e4f3e3fdccca38
pramodbontha <pramodkumar.bontha@gmail.com>
2021-08-09 11:21:26 +0200

    #5 Added a dialog for product card to show additional images

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,9 +3,7 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 
-interface HeaderProps {}
-
-const Header: React.FC<HeaderProps> = ({}) => {
+const Header = () => {
   return (
     <AppBar position="static">
       <Toolbar>
